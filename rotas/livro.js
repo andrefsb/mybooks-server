51d2974a95d18fe965581745abc89c1f98524f4d
André Sampaio <andresampaio@id.uff.br>
2023-07-27 13:55:32 -0300

    Routes for CRUD

--- rotas/livro.js
+++ rotas/livro.js
@@ -3,7 +3,19 @@ const { Router } = require("express");
 const router = Router();
 
 router.get('/', (req, res) =>{
-    res.send("Livros")
+    res.send("GET Livros")
+});
+
+router.post('/', (req, res) =>{
+    res.send("POST Livro")
+});
+
+router.patch('/', (req, res) =>{
+    res.send("PATCH Livro")
+});
+
+router.delete('/', (req, res) =>{
+    res.send("DELETE Livro")
 });
 
 module.exports = router;
