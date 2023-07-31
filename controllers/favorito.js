6484a520543153ff66517294f961f6fca42c1915
André Sampaio <andresampaio@id.uff.br>
2023-07-31 11:16:31 -0300

    Controllers and routes for favoritos

---
+++ controllers/favorito.js
@@ -0,0 +1,47 @@
+const { getTodosFavoritos, insereFavorito, deletaFavoritoPorId } = require("../services/favorito");
+
+function getFavoritos(req, res){
+    try{
+        const livros = getTodosFavoritos();
+        res.send(livros)
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postFavorito(req, res){
+    try{
+        const id = req.params.id;
+        insereFavorito(id);
+        res.status(201);
+        res.send("Book successfully added to favorites!")
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteFavorito(req, res){
+    try{
+        const id = req.params.id;
+
+        if(id && Number(id)) {
+            deletaFavoritoPorId(id);
+            res.send("Book successfully removed from favorites!");
+         }else{
+            res.status(422);
+            res.send("Invalid Id.")
+         }deleteFavorito
+        
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+module.exports={
+    getFavoritos,
+    postFavorito,
+    deleteFavorito
+}
